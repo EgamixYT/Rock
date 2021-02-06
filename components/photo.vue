6e1ba3c8bb5608bb2c4798ddf171d0b7b3265eca
Egamix <55224974+EgamixYT@users.noreply.github.com>
2021-02-06 21:34:40 +0200

    first commit

---
+++ components/photo.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    
+    <b-img src="https://images.unsplash.com/photo-1586627161720-ee2849303aee?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1263&q=80" class="img-fluid" alt="Responsive image"></b-img>
+   
+   
+    <h1></h1>
+   
+   
+    <b-img src="https://images.unsplash.com/photo-1595931288159-8164ae5def72?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1263&q=80" class="img-fluid" alt="Responsive image"></b-img>
+    
+    
+    <h1></h1>
+    
+    
+    <b-img src="https://images.unsplash.com/photo-1454618454636-a42c7330ccce?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1263&q=80" class="img-fluid" alt="Responsive image"></b-img>
+
+   
+  
+    
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        mainProps: {
+          center: true,
+          fluidGrow: true,
+          blank: true,
+          blankColor: '#bbb',
+          width: 600,
+          height: 400,
+          class: 'my-5'
+        }
+      }
+    },
+    methods: {
+      getImageUrl(imageId) {
+        const { width, height } = this.mainProps
+        return `https://picsum.photos/${width}/${height}/?image=${imageId}`
+      }
+    }
+  }
+</script>
---
+++ layouts/default.vue
@@ -0,0 +1,30 @@
+<template>
+  <div secondary >
+    
+    <Navbar/>
+    <Photo />
+    
+    <Footer />
+<Nuxt />
+  </div>
+</template>
+
+<script>
+import Navbar from '@/components/Navbar/'
+import Photo from '@/components/photo/'
+import Card from '@/components/Card'
+import Footer from '@/components/Footer'
+
+export default {
+  components: {
+    Navbar,
+    Photo,
+    Card,
+    Footer
+  }
+}
+</script>
+
+<style>
+
+</style>
