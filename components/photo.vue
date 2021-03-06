715159de047c1ac0d80f45c98c6a41828baa9f06
Egamix <55224974+EgamixYT@users.noreply.github.com>
2021-03-06 16:08:18 +0200

    photo and boulder changes

--- components/photo.vue
+++ components/photo.vue
@@ -3,23 +3,23 @@
     <h1></h1>
 
     <main>
-      <mdb-parallax
-        src="https://images.unsplash.com/photo-1578763399089-42091eda1cda?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1512&q=80"
+   
+        <img src="https://images.unsplash.com/photo-1578763399089-42091eda1cda?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1512&q=80"
         height="600"
-        factor="0.5"
-      >
-        </mdb-parallax>
+        class="img-fluid">
+      
+      
 
-      <mdb-mask dark overlay="blue-grey-light" flexCenter>
+      <mdb-mask dark overlay="blue-grey-light text-center" flexCenter>
         <mdb-row class="mb-5 pb-5 text-center">
-          <h1 class="justify-content: center baseline">
+          <h1 class=" mt-5 pt-1 mb-4 mx-auto" >
 
-              <p class="mt-5 pt-1 mb-4">Folow Us in:</p>
+           Folow Us in:
             </h1>
-          <mdb-col md="12" xl="12" class="mx-auto ">
+          <mdb-col md="12" xl="12" class="mx-auto">
             
         
-            <mdb-btn class="btn-fb" icon="facebook-f" href="facebook.com" fab>Facebook</mdb-btn>
+            <mdb-btn class="btn-fb zoom overlay" icon="facebook-f" href="facebook.com" fab>Facebook</mdb-btn>
             <mdb-btn class="btn-tw" icon="twitter" fab>Twitter</mdb-btn>
             <mdb-btn class="btn-gplus" icon="google-plus-g" href="google.com" fab> Google +</mdb-btn>
             <mdb-btn class="btn-li" icon="linkedin-in" fab>Linkedin</mdb-btn>
@@ -27,20 +27,37 @@
           </mdb-col>
         </mdb-row>
       </mdb-mask>
+      <mdb-row>
+        <mdb-col col="12">
+           <img src="https://images.unsplash.com/photo-1595931288159-8164ae5def72?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80"
+    
+        class="mb-2 img-fluid"
+        />
+        </mdb-col>
+        <mdb-col col="6"> <img src="https://images.unsplash.com/photo-1583623010148-9c00f6b12efe?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1534&q=80"
+        height=""
+        
+        class="float-left img-fluid mb-2"
 
-      <mdb-parallax
-        src="https://images.unsplash.com/photo-1595931288159-8164ae5def72?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80"
-        factor="1.0"
+        /></mdb-col>
+        <mdb-col col="6"><img src="" alt=""></mdb-col>
+        <mdb-col col="12"></mdb-col>
+      </mdb-row>
+     
+      
+  
+      
+       
+     
+        <img src="https://images.unsplash.com/photo-1574002493833-92a3c9e6580f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80"
+       
         height="600"
-      />
-      <mdb-mask dark overlay="blue-grey-light" flexCenter>
+        class="img-fluid"
+        
+        />
 
-      </mdb-mask>
-      <mdb-parallax
-        src="https://images.unsplash.com/photo-1574002493833-92a3c9e6580f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80"
-        factor="0.5"
-        height="600"
-      />
+       
+    
     </main>
    
   </div>
